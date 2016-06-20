9fae72f30305f0d9ebfbb3e5eeb2f7c24384c35f
MichalZahir <michaelzahir@gmail.com>
2016-06-20 16:02:59 +0200

    Adding the literature category question handler.

--- app/src/main/java/com/example/michalzahir/pagk16/adapter/RecyclerAdapter.java
+++ app/src/main/java/com/example/michalzahir/pagk16/adapter/RecyclerAdapter.java
@@ -21,6 +21,7 @@ import com.example.michalzahir.pagk16.CATEGORY_QUESTIONS.ASTRONOMY_QUESTIONS;
 import com.example.michalzahir.pagk16.CATEGORY_QUESTIONS.CHEMISTRY_QUESTIONS;
 import com.example.michalzahir.pagk16.CATEGORY_QUESTIONS.GEOGRAPHY_QUESTIONS;
 import com.example.michalzahir.pagk16.CATEGORY_QUESTIONS.HISTORY_QUESTIONS;
+import com.example.michalzahir.pagk16.CATEGORY_QUESTIONS.LITERATURE_QUESTIONS;
 import com.example.michalzahir.pagk16.CATEGORY_QUESTIONS.SPORT_QUESTIONS;
 import com.example.michalzahir.pagk16.ConstantsClass;
 import com.example.michalzahir.pagk16.QUESTIONS;
@@ -142,6 +143,8 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.MyView
                 get_geography_questions();
             else if (item.getCategory()=="Astronomy")
                 get_astronomy_questions();
+            else if (item.getCategory()=="Literature")
+                get_literature_questions();
             else {
                 Random rn = new Random();
                 int ID = rn.nextInt(ConstantsClass.QuestionsQuestSize) + 1;
@@ -473,6 +476,67 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.MyView
                     Backendless.Persistence.of( ASTRONOMY_QUESTIONS.class ).findById(q.getObjectId(), new AsyncCallback<ASTRONOMY_QUESTIONS>() {
                         @Override
                         public void handleResponse(ASTRONOMY_QUESTIONS response) {
+                            // a Contact instance has been found by ObjectId
+                            Bundle insideBundle = new Bundle();
+                            System.out.println("this is the question from the backendless DB  "+response.getQuestion()
+                                    +".    this is the first answer   "+response.getAnswer_a()+".   Hurrraaa success !!!!"+response.getCORRECT_A()+" B boolean:"+response.getCORRECT_B()+" D boolean:"+response.getCORRECT_D()+" C boolean:"+response.getCORRECT_C()+"AA"+response.getAnswer_a()+"bA"+response.getANSWER_B()+"cA"+response.getANSWER_C()+"DA"+response.getANSWER_D());
+                            insideBundle.putString("Question",response.getQuestion());
+                            insideBundle.putString("Answer_A",response.getAnswer_a());
+                            insideBundle.putString("Answer_B",response.getANSWER_B());
+                            insideBundle.putString("Answer_C",response.getANSWER_C());
+                            insideBundle.putString("Answer_D",response.getANSWER_D());
+                            insideBundle.putBoolean("correct_A",response.getCORRECT_A());
+                            insideBundle.putBoolean("correct_B",response.getCORRECT_B());
+                            insideBundle.putBoolean("correct_C",response.getCORRECT_C());
+                            insideBundle.putBoolean("correct_D",response.getCORRECT_D());
+                            List<QUESTIONS> savedQuestions =  new ArrayList<>();
+                            savedquestions = new SavedQuestions(savedQuestions);
+                            savedquestions.addToSavedQuestions(response);
+                            Log.d(TAG, "trying to fetch questions from DB inside the handle Response method   " +insideBundle);
+                            //StartActivity(bundle ,context);
+                            System.out.println("bundle from the middle tier : "+insideBundle);
+                            Intent i = new Intent(context, questionActivity.class);
+                            i.putExtras(insideBundle);
+                            context.startActivity(i);
+
+                        }
+
+                        @Override
+                        public void handleFault(BackendlessFault fault) {
+                            Log.d(TAG, "fault trying to fetch questions from DB fault" + fault.getMessage()+fault.getCode()+fault.getDetail()+fault.getClass());
+                        }
+                    });
+                }
+            }
+
+            @Override
+            public void handleFault(BackendlessFault fault) {
+                Log.d(TAG, "fault trying to fetch questions from DB fault" + fault.getMessage()+fault.getCode()+fault.getDetail()+fault.getClass());
+
+            }});
+
+
+
+
+    }
+    public void get_literature_questions(){
+
+        Random rn = new Random();
+        int ID = rn.nextInt(ConstantsClass.LiteratureQuestSize) + 1;
+        System.out.println(ID);
+        String whereClause = " ID="+ID;
+        BackendlessDataQuery dataQuery = new BackendlessDataQuery();
+        dataQuery.setWhereClause(whereClause);
+
+        Backendless.Persistence.of( LITERATURE_QUESTIONS.class ).find(dataQuery, new AsyncCallback<BackendlessCollection<LITERATURE_QUESTIONS>>() {
+            @Override
+            public void handleResponse(BackendlessCollection<LITERATURE_QUESTIONS> foundQuestions){
+                for( LITERATURE_QUESTIONS q : foundQuestions.getData() )
+                {
+                    //System.out.println(  " The shit  in the table :  '"+ q.getObjectId()) ;
+                    Backendless.Persistence.of( LITERATURE_QUESTIONS.class ).findById(q.getObjectId(), new AsyncCallback<LITERATURE_QUESTIONS>() {
+                        @Override
+                        public void handleResponse(LITERATURE_QUESTIONS response) {
                             // a Contact instance has been found by ObjectId
                             Bundle insideBundle = new Bundle();
                             System.out.println("this is the question from the backendless DB  "+response.getQuestion()
