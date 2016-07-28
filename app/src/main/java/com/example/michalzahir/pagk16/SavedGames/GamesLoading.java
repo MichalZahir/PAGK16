a52c2919315dd26520e5830bb602629995e25f8c
MichalZahir <michaelzahir@gmail.com>
2016-07-28 10:58:36 +0200

    Able to load the games into the ListView from the DB. Fixed a bug with returnning to the profile activity From the saved games.

--- app/src/main/java/com/example/michalzahir/pagk16/SavedGames/GamesLoading.java
+++ app/src/main/java/com/example/michalzahir/pagk16/SavedGames/GamesLoading.java
@@ -7,6 +7,7 @@ import android.util.Log;
 import com.backendless.Backendless;
 import com.backendless.BackendlessCollection;
 import com.backendless.async.callback.AsyncCallback;
+import com.backendless.exceptions.BackendlessException;
 import com.backendless.exceptions.BackendlessFault;
 import com.backendless.persistence.BackendlessDataQuery;
 import com.example.michalzahir.pagk16.ConstantsClass;
@@ -14,6 +15,7 @@ import com.example.michalzahir.pagk16.QUESTIONS;
 import com.example.michalzahir.pagk16.playerObejtID;
 import com.example.michalzahir.pagk16.questionActivity;
 
+import java.util.ArrayList;
 import java.util.Random;
 
 /**
@@ -22,40 +24,82 @@ import java.util.Random;
 public class GamesLoading {
 
     private static final String TAG = "Saved Games Loading";
-
+    static ArrayList<Saved_Games> SavedGameslist = new ArrayList<Saved_Games>();
     static  void loadSavedGames(){
-
-
-        String whereClause = "firstUserID='"+ playerObejtID.getUserObjectID()+"'";
-        BackendlessDataQuery dataQuery = new BackendlessDataQuery();
+        SavedGameslist.clear();
+        Log.d(TAG, "player object ID b4 the query " + playerObejtID.getUserObjectID());
+        String whereClause = "firstUserID='"+ playerObejtID.getUserObjectID()+"'"+"OR secondUserID='"+ playerObejtID.getUserObjectID()+"'";
+        final BackendlessDataQuery dataQuery = new BackendlessDataQuery();
         dataQuery.setWhereClause(whereClause);
+        final BackendlessCollection<Saved_Games>[] foundGames = new BackendlessCollection[]{null};
 
-        Backendless.Persistence.of( Saved_Games.class ).find(dataQuery, new AsyncCallback<BackendlessCollection<Saved_Games>>() {
+        Thread thread = new Thread(new Runnable()
+        {
             @Override
-            public void handleResponse(BackendlessCollection<Saved_Games> foundGames){
-                for( Saved_Games q : foundGames.getData() )
+            public void run()
+            {
+                try
                 {
-                     Backendless.Persistence.of( Saved_Games.class ).findById(q.getObjectId(), new AsyncCallback<Saved_Games>() {
-                        @Override
-                        public void handleResponse(Saved_Games response) {
+                    try {
+                        foundGames[0] = Backendless.Persistence.of( Saved_Games.class ).find(dataQuery);
+                    } catch (BackendlessException e) {
+                        Log.d(TAG, "fault trying to fetch The saved Games  from DB fault" + e.getMessage()+e.getCode()+e.getDetail()+e.getClass());
+                        e.printStackTrace();
+                    }
 
+                    try {
+                        for( Saved_Games q : foundGames[0].getData() )
+                        {
+                            Saved_Games response = Backendless.Persistence.of( Saved_Games.class ).findById(q.getObjectId());
                             Log.d(TAG, "The saved Games were Found  First user ID  " +response.getFirstUserID()+" SecondUserID" +response.getSecondUserID() + " Saved Games Object ID:" +response.getObjectId());
-
+                            SavedGameslist.add(response);
                         }
-
-                        @Override
-                        public void handleFault(BackendlessFault fault) {
-                            Log.d(TAG, "fault trying to fetch The saved Games  from DB fault" + fault.getMessage()+fault.getCode()+fault.getDetail()+fault.getClass());
-                        }
-                    });
+                    } catch (BackendlessException e) {
+                        e.printStackTrace();
+                        Log.d(TAG, "fault trying to fetch The saved Games  from DB fault" + e.getMessage()+e.getCode()+e.getDetail()+e.getClass());
+                    }
+                }
+                catch (Exception e)
+                {
+                    e.printStackTrace();
                 }
             }
+        });
 
-            @Override
-            public void handleFault(BackendlessFault fault) {
-                Log.d(TAG, "fault trying to fetch saved Games from DB fault" + fault.getMessage()+fault.getCode()+fault.getDetail()+fault.getClass());
+        thread.start();
+        try {
+            thread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 
-            }});
+
+//        Backendless.Persistence.of( Saved_Games.class ).find(dataQuery, new AsyncCallback<BackendlessCollection<Saved_Games>>() {
+//            @Override
+//            public void handleResponse(BackendlessCollection<Saved_Games> foundGames){
+//                for( Saved_Games q : foundGames.getData() )
+//                {
+//                     Backendless.Persistence.of( Saved_Games.class ).findById(q.getObjectId(), new AsyncCallback<Saved_Games>() {
+//                        @Override
+//                        public void handleResponse(Saved_Games response) {
+//                            SavedGameslist.add(response);
+//                            Log.d(TAG, "The saved Games were Found  First user ID  " +response.getFirstUserID()+" SecondUserID" +response.getSecondUserID() + " Saved Games Object ID:" +response.getObjectId());
+//
+//                        }
+//
+//                        @Override
+//                        public void handleFault(BackendlessFault fault) {
+//                            Log.d(TAG, "fault trying to fetch The saved Games  from DB fault" + fault.getMessage()+fault.getCode()+fault.getDetail()+fault.getClass());
+//                        }
+//                    });
+//                }
+//            }
+//
+//            @Override
+//            public void handleFault(BackendlessFault fault) {
+//                Log.d(TAG, "fault trying to fetch saved Games from DB fault" + fault.getMessage()+fault.getCode()+fault.getDetail()+fault.getClass());
+//
+//            }});
 
 
 
--- app/src/main/java/com/example/michalzahir/pagk16/SavedGames/SavedGamesActivity.java
+++ app/src/main/java/com/example/michalzahir/pagk16/SavedGames/SavedGamesActivity.java
@@ -1,24 +1,51 @@
 package com.example.michalzahir.pagk16.SavedGames;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import com.example.michalzahir.pagk16.Profile2_ScrollingActivity;
 import com.example.michalzahir.pagk16.R;
 
 public class SavedGamesActivity extends AppCompatActivity {
+    private static final String TAG = "SavedGamesActivity";
     String[] mobileArray = {"Android","IPhone","WindowsMobile","Blackberry","WebOS","Ubuntu","Windows7","Max OS X", "asdasd","asd","asd","asd","asd", "asd","qweqwe","qwe","qwe","qwe","qwe","qwe","qwe"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_saved_games);
+        GamesLoading.loadSavedGames();
+        int i =1;
+        int d =0;
+        String [] SavedGamesArray = new String[GamesLoading.SavedGameslist.size()];
+        Log.d(TAG, "THe moment before going into the loop for after loading the games");
+         for (Saved_Games sg :GamesLoading.SavedGameslist){
+          SavedGamesArray[d]= "Game: " +i ;
+             Log.d(TAG, "The saved Games Array object ID = " + sg.getObjectId() + "who's turn" +sg.getWhosTurn());
 
-        ArrayAdapter adapter = new ArrayAdapter<String>(this, R.layout.activity_listview, mobileArray);
+
+
+
+             i++;
+             d++;
+
+         }
+
+
+        ArrayAdapter adapter = new ArrayAdapter<String>(this, R.layout.activity_listview, SavedGamesArray);
 
         ListView listView = (ListView) findViewById(R.id.mobile_list);
         listView.setAdapter(adapter);
-        GamesLoading.loadSavedGames();
+     }
+    @Override
+    public void onBackPressed() {
+        Intent i = new Intent(getApplicationContext(),
+                Profile2_ScrollingActivity.class);
+         startActivity(i);
+        finish();
     }
 }
