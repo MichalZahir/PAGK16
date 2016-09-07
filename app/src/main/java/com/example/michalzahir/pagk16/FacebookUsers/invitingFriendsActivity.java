938659a1ac304e7bcb82b4a925e04989eb4b6b8e
MichalZahir <michaelzahir@gmail.com>
2016-09-07 17:06:10 +0200

    Sovled the problem with going back to the profile after inviting friends from facebook to play the game.

--- app/src/main/java/com/example/michalzahir/pagk16/FacebookUsers/invitingFriendsActivity.java
+++ app/src/main/java/com/example/michalzahir/pagk16/FacebookUsers/invitingFriendsActivity.java
@@ -1,10 +1,14 @@
 package com.example.michalzahir.pagk16.FacebookUsers;
 
+import android.content.Intent;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
+import com.example.michalzahir.pagk16.Profile2_ScrollingActivity;
 import com.example.michalzahir.pagk16.R;
 import com.facebook.FacebookSdk;
 import com.facebook.share.model.AppInviteContent;
@@ -13,11 +17,15 @@ import com.facebook.share.widget.AppInviteDialog;
 import bolts.AppLinks;
 
 public class invitingFriendsActivity extends AppCompatActivity {
+    private Button backToProfile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inviting_friends);
+        backToProfile = (Button) findViewById(R.id.backToProfileButton);
+
+
         // app link url
         FacebookSdk.sdkInitialize(getApplicationContext());
         Uri targetUrl = AppLinks.getTargetUrlFromInboundIntent(this, getIntent());
@@ -40,5 +48,20 @@ public class invitingFriendsActivity extends AppCompatActivity {
                     .setPreviewImageUrl(previewImageUrl)
                     .build();
             AppInviteDialog.show(this, content);
-        } }
+        }
+
+
+        backToProfile.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View view) {
+                Intent i = new Intent( invitingFriendsActivity.this, Profile2_ScrollingActivity.class);
+                startActivity(i);
+                finish();
+            }
+
+        });
+
+
+
+    }
 }
